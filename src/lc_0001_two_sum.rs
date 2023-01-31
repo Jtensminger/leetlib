9b449ecae30fb98b2e922ea70544e0e7d028e49a
Joseph Ensminger <joetensminger@gmail.com>
2023-01-31 13:15:32 -0600

    applied hashmaps to two sum

--- src/lc_0001_two_sum.rs
+++ src/lc_0001_two_sum.rs
@@ -1,36 +1,51 @@
+use std::collections::HashMap;
+
+// BRUTE FORCE
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.len() - 1 {
-        for j in i + 1..nums.len() {
-            if nums[i] + nums[j] == target {
-                return Vec::from([i as i32, j as i32]);
-            }
-        }
-    }
-    Vec::<i32>::new()
+	
+	let mut hm: HashMap<i32, i32> = HashMap::new();
+
+	// iterate through numbers
+	for (index, num) in nums.into_iter().enumerate() {
+		let complement = target - num;
+
+		if let Some(comp_index) = hm.get(&complement) {
+			return vec![*comp_index, index as i32]
+		}
+
+		hm.entry(num).or_insert(index as i32);
+
+	}
+	
+	vec![]
 }
+
+
+
+
 
 #[cfg(test)]
 mod test {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn ext1() {
-        assert_eq!(two_sum(vec![11, 2, 15, 7], 9), vec![0, 1])
-    }
+	#[test]
+	fn ext1() {
+		assert_eq!(two_sum(vec![11, 2, 15, 7], 9), vec![1, 3])
+	}
 
-    #[test]
-    fn ext2() {
-        assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1])
-    }
+	#[test]
+	fn ext2() {
+		assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1])
+	}
 
-    #[test]
-    fn ext3() {
-        assert_eq!(two_sum(vec![3, 7, 0, 1], 7), vec![1, 2])
-    }
+	#[test]
+	fn ext3() {
+		assert_eq!(two_sum(vec![3, 7, 0, 1], 7), vec![1, 2])
+	}
 
-    #[test]
-    fn ext4() {
-        assert_eq!(two_sum(vec![3, 2, 3], 6), vec![0, 2])
-    }
+	#[test]
+	fn ext4() {
+		assert_eq!(two_sum(vec![3, 2, 3], 6), vec![0, 2])
+	}
 }
  
