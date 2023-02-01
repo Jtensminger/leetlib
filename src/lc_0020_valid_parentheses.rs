8916366a6ee3366e3b93adede1ce2ea918aa75da
Joseph Ensminger <joetensminger@gmail.com>
2023-01-31 21:13:24 -0600

    lc valid parentheses problem completed

---
+++ src/lc_0020_valid_parentheses.rs
@@ -0,0 +1,94 @@
+pub fn is_valid(s: String) -> bool {
+	if s.len() % 2 != 0 || s.len() == 0 {
+		return false
+	}
+
+	let mut peekable = s.chars().peekable();
+	let mut stack: Vec<char> = vec![];
+
+	while peekable.peek() != None {
+		let c = peekable.next();
+		match c {
+			Some(n @ '{') => if peekable.peek() == None { return false } else { stack.push(n); },
+			Some(n @ '(') => if peekable.peek() == None { return false } else { stack.push(n); },
+			Some(n @ '[') => if peekable.peek() == None { return false } else { stack.push(n); },
+			Some('}') => if stack.last() != Some(&'{') { return false } else { stack.pop(); },
+			Some(')') => if stack.last() != Some(&'(') { return false } else { stack.pop(); },
+			Some(']') => if stack.last() != Some(&'[') { return false } else { stack.pop(); },
+			_ => return false,
+		}
+	}
+	if stack.len() != 0 {
+		return false;
+	}
+
+	true
+}
+
+		// match c {
+		// 	Some('{') => if peekable.find(|&x| x == '}') == None {return false},
+		// 	Some('(') => if peekable.find(|&x| x == ')') == None {return false},
+		// 	Some('[') => if peekable.find(|&x| x == ']') == None {return false},
+		// 	_ => return false,
+		// }
+
+		// match c {
+		// 	'{' => if (n != '}') {return false},
+		// 	'(' => if (n != ')') {return false},
+		// 	'[' => if (n != ']') {return false},
+		// 	_ => return false,
+		// }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn ext1() {
+		assert_eq!(is_valid(String::from("()")), true)
+	}
+
+	#[test]
+	fn ext2() {
+		assert_eq!(is_valid(String::from("()[]{}")), true)
+	}
+
+	#[test]
+	fn ext3() {
+		assert_eq!(is_valid(String::from("(]")), false)
+	}
+
+	#[test]
+	fn ext4() {
+		assert_eq!(is_valid(String::from(")(")), false)
+	}
+
+	#[test]
+	fn ext5() {
+		assert_eq!(is_valid(String::from("{}(")), false)
+	}
+	#[test]
+	fn ext6() {
+		assert_eq!(is_valid(String::from("{[]}")), true)
+		// {[
+	}
+	#[test]
+	fn ex7() {
+		assert_eq!(is_valid(String::from("{[(])}")), false)
+	}
+
+	#[test]
+	fn ex8() {
+		assert_eq!(is_valid(String::from("((")), false)
+	}
+
+	#[test]
+	fn ex9() {
+		assert_eq!(is_valid(String::from("))")), false)
+	}
+	#[test]
+	fn ext10() {
+		assert_eq!(is_valid(String::from("[[[]")), false)
+	}
+}
+ 
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+pub mod lc_0020_valid_parentheses;
 pub mod lc_0001_two_sum;
 pub use lc_0001_two_sum::*;
+pub use lc_0020_valid_parentheses::*;
