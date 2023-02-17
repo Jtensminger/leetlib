55b0b291a4d0cd28524ec535c397c41180be1ab2
Joseph Ensminger <joetensminger@gmail.com>
2023-02-16 21:52:27 -0600

    reimplemented LC 0121 using iterators .fold()

--- src/lc_0121_best_time_to_buy_and_sell_stock.rs
+++ src/lc_0121_best_time_to_buy_and_sell_stock.rs
@@ -7,21 +7,37 @@
         Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
  */
 
+// Bruteforce
+// pub fn max_profit(prices: Vec<i32>) -> i32 {
+//         let mut highest_profit_potential = 0;
+//         let num_of_days = prices.len();
+
+//         for i in 0..num_of_days - 1 {
+//                 for n in i+1..num_of_days {
+//                         let profit = prices[n] - prices[i];
+
+//                         if profit >= highest_profit_potential {
+//                                 highest_profit_potential = profit;        
+//                         }
+//                 }
+//         }
+//         highest_profit_potential
+// }
+
+// Single pass
 pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut highest_profit_potential = 0;
-        let num_of_days = prices.len();
+        prices
+                .iter()
+                .fold((0, i32::MAX), |(mut max_profit, mut prev_lowest_price), current_price| {
+                        dbg!(max_profit); dbg!(prev_lowest_price); dbg!(current_price);
 
-        for i in 0..num_of_days - 1 {
-                for n in i+1..num_of_days {
-                        let profit = prices[n] - prices[i];
+                        prev_lowest_price = i32::min(*current_price, prev_lowest_price);
+                        max_profit = i32::max(max_profit, current_price-prev_lowest_price);
 
-                        if profit >= highest_profit_potential {
-                                highest_profit_potential = profit;        
-                        }
-                }
-        }
-        highest_profit_potential
+                        dbg!((max_profit, prev_lowest_price))
+                }).0
 }
+
 
 #[cfg(test)]
 mod test {
