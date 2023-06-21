e0565af615bd4b9d244766d8623d095ae1d2ee0d
Joseph Ensminger <joetensminger@gmail.com>
2023-06-21 15:46:25 -0500

    optimized 3sum

--- src/lc_0015_3sum.rs
+++ src/lc_0015_3sum.rs
@@ -5,7 +5,7 @@
                 sum to zero => i + j + k == 0
 */
 use std::collections::HashSet;
-pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+pub fn naive_three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         /* find all possible triplets (e.g index combinations) & store the valid ones */
         let mut valid_triplets: HashSet<Vec<i32>> = HashSet::with_capacity(nums.capacity());
         for i in 0..nums.len() {
@@ -29,6 +29,44 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         valid_triplets.into_iter().collect::<Vec<Vec<i32>>>()
 }
 
+use std::cmp::Ordering::{Equal, Greater, Less};
+pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut nums = nums;
+        nums.sort();
+        let mut ans: Vec<Vec<i32>> = Vec::new();
+
+        for i in 0..nums.len() {
+                /* skip duplicate numbers */
+                if i > 0 && nums[i] == nums[i - 1] { continue; }
+                /* create left & right pointers */
+                let (mut l, mut r) = (i + 1, nums.len() - 1);
+                /* narrow the range to find triplets */
+                while l < r {
+                        match (nums[i] + nums[l] + nums[r]).cmp(&0) {
+                                /* increase sum towards zero */
+                                Less => l += 1, 
+                                /* decrease sum towards zero */
+                                Greater => r -= 1,
+                                /* found valid triplet */
+                                Equal => {
+                                        ans.push(vec![nums[i], nums[l], nums[r]]);
+                                        /* increase sum towards zero while skipping duplicate numbers */
+                                        l += 1;
+                                        while nums[l] == nums[l - 1] && l < r {
+                                                l += 1;
+                                        }
+                                        /* decrease sum towards zero while skipping duplicate numbers */
+                                        r -= 1;
+                                        while nums[r] == nums[r + 1] && l < r {
+                                                r -= 1;
+                                        }
+                                }
+                        }
+                }
+        }
+
+        ans
+}
 #[cfg(test)]
 mod tests {
         use super::*;
