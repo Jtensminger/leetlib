c25102658aedd0978b4067011b577b898e01c8ec
Joseph Ensminger <joetensminger@gmail.com>
2023-07-24 13:21:47 -0500

    optimal solution to lc_0424

--- src/lc_0424_longest_repeating_character_replacement.rs
+++ src/lc_0424_longest_repeating_character_replacement.rs
@@ -4,22 +4,25 @@
         You can perform this operation at most k times.
         Return the length of the longest substring containing the same letter you can get after performing the above operations.
 */
-use std::collections::HashMap;
 pub fn character_replacement(s: String, k: i32) -> i32 {
-        let s: Vec<char> = s.chars().collect();
-        let (mut res, mut l, mut maxf) = (0, 0, 0);
-        let mut count: HashMap<char, u64> = HashMap::new();
-        for r in 0..s.len() {
-            *count.entry(s[r]).or_default() += 1;
-            maxf = maxf.max(*count.get(&s[r]).unwrap());
-
-            while (r - l + 1) - maxf as usize > k as usize {
-                *count.get_mut(&s[l]).unwrap() -= 1;
-                l += 1;
-            }
-            res = res.max(r - l + 1);
+        let s: Vec<char> = s.chars().into_iter().collect();
+        let mut counts = [0; 26];
+        let (mut max_sub, mut max_ch, mut start) = (0, 0, 0);
+        for end in 0..s.len() {
+                /* increment char frequency & store most frequent character in window */
+                let ascii = s[end] as usize - 'A' as usize;
+                counts[ascii] += 1; 
+                max_ch = max_ch.max(counts[ascii]);    
+                /* determine # of replacements needed to make the window all the same character */
+                /* false => invalid window => shrink window */
+                while (end - start + 1) - max_ch as usize > k as usize { 
+                        let ascii = s[start] as usize - 'A' as usize;
+                        counts[ascii] -= 1;
+                        start += 1;
+                }
+                max_sub = max_sub.max(end - start + 1);
         }
-        res as i32
+        max_sub as i32
 }
 
 #[cfg(test)]
@@ -28,9 +31,9 @@ pub mod tests {
 
         #[test]
         fn ext1() {
-                // assert_eq!(4, character_replacement("ABAB".to_string(), 2));
+                assert_eq!(4, character_replacement("ABAB".to_string(), 2));
                 assert_eq!(4, character_replacement("AABABBA".to_string(), 1));
-                // assert_eq!(4, character_replacement("AAAA".to_string(), 2));
-                // assert_eq!(4, character_replacement("ABBB".to_string(), 2));
+                assert_eq!(4, character_replacement("AAAA".to_string(), 2));
+                assert_eq!(4, character_replacement("ABBB".to_string(), 2));
         }
 }
