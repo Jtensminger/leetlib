627b3708fa4f087acb6e4fddda16912ce970e95b
Joseph Ensminger <joetensminger@gmail.com>
2023-07-27 16:15:53 -0500

    updated semantics of binary serach

--- src/lc_0704_binary_search.rs
+++ src/lc_0704_binary_search.rs
@@ -3,13 +3,13 @@
 use std::cmp::Ordering::{Greater,Equal,Less};
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {        
         if nums.len() == 0 { return -1 }
-        let (mut start, mut end) = (0, nums.len());
-        while start < end {
-                let mid = start + (end - start) / 2;
+        let (mut left, mut right) = (0, nums.len());
+        while left < right {
+                let mid = left + (right - left) / 2;
                 match &target.cmp(&nums[mid]) {
                         Equal => return mid as i32,
-                        Less => end = mid,
-                        Greater => start = mid + 1
+                        Less => right = mid,
+                        Greater => left = mid + 1
                 }
         }
         -1
