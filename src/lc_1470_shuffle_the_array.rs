1ac8fbd8674c8598aa1f87082aa290469b585ed5
Joseph Ensminger <joetensminger@gmail.com>
2023-12-12 07:44:23 -0600

    completed lc_1470_shuffle_the_array

--- src/lc_1470_shuffle_the_array.rs
+++ src/lc_1470_shuffle_the_array.rs
@@ -1,26 +1,17 @@
-/* 
-    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
-    Return the array in the form [x1,y1,x2,y2,...,xn,yn]
-*/
 pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
         let n = n as usize;
-        let mut result = vec![0; nums.len()];
-
+        let mut res = Vec::with_capacity(2*n);
         for i in 0..n {
-            result[2 * i] = nums[i];
-            result[2 * i + 1] = nums[n + i];
+            res.push(nums[i]);
+            res.push(nums[i+n]);
         }
-
-        result
+        res
 }
-
-#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn ext1() {
-        let (n, nums) = (3, vec![2,5,1,3,4,7]);
-        assert_eq!(vec![2,3,5,4,1,7], shuffle(nums, n));
+        assert_eq!(shuffle(vec![2,5,1,3,4,7], 3), vec![2,3,5,4,1,7])
     }
 }
